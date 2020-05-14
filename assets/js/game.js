b38a0aeaa3fd480d0c6158921789baaa12813f88
CRSPN <doeraysme@gmail.com>
2020-05-14 14:43:32 -0600

    dinked with loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,15 +3,18 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 
-var fight = function() {
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+//    * Fight all enemy robots
+//    * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
+var fight = function(enemyName) {
 
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
@@ -23,7 +26,7 @@ var fight = function() {
         enemyHealth = enemyHealth - playerAttack;
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-        )
+        );
         
         playerHealth = playerHealth - enemyAttack
         // check enemies health
@@ -56,5 +59,8 @@ var fight = function() {
         window.alert("You need to pick a valid option. Try again!");
     }
 };
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
 
-fight();
+//fight();
