7612b0c869a5a2bccb516f02d8da9474bd62e058
CRSPN <doeraysme@gmail.com>
2020-05-18 17:38:15 -0600

    java is easier now. makes sense again

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,7 +70,6 @@ var startGame = function() {
     for(var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             alert("Welcome to Battlebots! Round " + ( i + 1 ));
-            debugger; 
 
             var pickedEnemyObj = enemyInfo[i]; 
             
@@ -146,9 +145,18 @@ var randomNumber = function(min, max) {
 
     return value;
 };
+var getPlayerName = function() {
+    var name = "";
+    
+        while (name === "" || name === null) {
+            name = prompt("What is your robot's name?");
+        }
 
+    console.log("Your robot's name is " + name);
+    return name;
+};
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 11,
     money: 10,
